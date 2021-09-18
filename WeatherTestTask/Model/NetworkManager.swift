1d028cd7d1f5a05305cdddd6c4b36df8ca9892c5
Sergey <piolersa85@mail.ru>
2021-09-19 01:33:10 +0200

    NS

---
+++ WeatherTestTask/Model/NetworkManager.swift
@@ -0,0 +1,59 @@
+//
+//  NetworkManager.swift
+//  WeatherTestTask
+//
+//  Created by Sergey on 18.09.2021.
+//
+
+import Foundation
+
+struct NetworkManager {
+    
+    func fetchWeather(complitionHandler: @escaping (Weather) -> Void) {
+        
+        let urlString = "https://api.weather.yandex.ru/v2/forecast?lat=59.932602&lon=30.347810"
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url, timeoutInterval: Double.infinity)
+        request.addValue("\(apiKey)", forHTTPHeaderField: "X-Yandex-API-Key")
+      
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let data = data  else {
+                print(String(describing: error))
+                return
+            }
+          //  print(String(data: data, encoding: .utf8)!)
+            if let weather = self.parseJSON(withData: data) {
+                
+                complitionHandler(weather)
+            }
+            
+        }
+        
+        task.resume()
+     
+    }
+    
+    
+    func parseJSON(withData data: Data) -> Weather? {
+        let decoder = JSONDecoder()
+        
+        
+        do {
+            
+            let weatherData = try decoder.decode(WeatherData.self, from: data)
+            guard let weather = Weather(weatherData: weatherData) else { return nil }
+            
+            return weather
+            
+        } catch  let error as NSError {
+            
+            print(error.localizedDescription)
+            
+        }
+        
+        return nil
+    }
+}
---
+++ WeatherTestTask/Service/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  WeatherTestTask
+//
+//  Created by Sergey on 18.09.2021.
+//
+
+import Foundation
+
+let apiKey = "f491b137-8800-4e67-96f4-25791096f780"
